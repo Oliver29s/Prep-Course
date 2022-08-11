693b9581fe596213c17def4341b44c94e33fdb1a
unknown <mawiis455@gmail.com>
2022-08-10 23:17:06 -0500

    Oliver bebe

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "oliver";
+const nuevaString = "cambios";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 10; 
@@ -12,7 +12,7 @@ const nuevoBool = null;
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
 
-// Resuelve el siguiente problema matemático:
+// Resuelve el siguiente problema matemático:t
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
